f3a9fdeaddcb739b849f35a601e5771ff2c157a6
RofifAthoyaNurPrabu <rofif.athoya.ft19@mail.umy.ac.id>
2022-01-06 01:52:21 +0700

    PWS

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    $("#name").text(profile.getName());
-    $("#email").text(profile.getEmail());
-    $("#image").attr('src', profile.getImageUrl());
-    $(".data").css("display", "block");
-    $(".g-signin2").css("display", "none");
+    localStorage.setItem("nama",profile.getName());
+    localStorage.setItem("image",profile.getImageUrl());
+    localStorage.setItem("email", profile.getEmail());
+    window.location.href="/data.html"
 }
 
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        alert("You have been signed out successfully");
-        $(".data").css("display", "none");
-        $(".g-signin2").css("display", "block");
-    });
+function onLoad(){
+    gapi.load("auth2", function(){
+        gapi.auth2.init();
+
+    })
 }
+
+
+$("#user").ready(function () {
+    var user = document.getElementById("user")
+    user.innerHTML = `Selamat Datang, ${localStorage.getItem("nama")}`
+});
+
+$("#tbl").ready(function () {
+    var tbl = document.getElementById("tbl")
+    getAll().then(response => {
+        console.log(response)
+        for(var i = 0; i <response.length; i++){
+            const tr = tbl.insertRow()
+            const td1 = tr.insertCell();
+            const td2 = tr.insertCell();
+            const td3 = tr.insertCell();
+            const td4 = tr.insertCell();
+            console.log(response[i])
+                // const aksi = tr. document.getElementById("tbl-aksi")
+                // aksi.innerHTML = 
+                
+                // td.innerHTML = response[i][Object.keys(response[i])[j]];
+            td1.innerHTML = response[i].id_indomie
+            td2.innerHTML = response[i].jenis
+            td3.innerHTML = response[i].harga
+            td4.innerHTML = `<div class ="justify content-center">
+            <a class="btn btn-warning ms-2" href="edit.html?id_indomie=${response[i].id_indomie}">Edit </a>
+            <button type ="button" class="btn btn-danger ms-2" onclick="del(${response[i].id_indomie});">Delete</button>
+            </div>`
+            }
+        }
+    )
+});
